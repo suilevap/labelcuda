dc504679a4a5a9cea98364604534f98b166821fe
suilevap@gmail.com <suilevap@gmail.com@e7e6688a-a52e-aa04-c7a9-1f37116e448b>
2010-06-04 08:20:44 +0000

    word finder ver 0.1 result: MB Cpu(s) Gpu(s) 4 0.1 1.2 8 0.2 1.4 16 0.4 1.3 32 0.7 1.6 64 1.5 1.8 128 2.9 2.3 256 5.8 3.2 512 11.7 5.3 1024 23.7 9.3 2048 48.4 17.0 4096 98.6 32.5

--- trunk/labelsCUDA/labelsCUDA/deviceWordsFinder.cu
+++ trunk/labelsCUDA/labelsCUDA/deviceWordsFinder.cu
@@ -121,73 +121,84 @@ device_NormalizeAllWords( unsigned int* words, size_t count)
 	}
 }
 
-void deviceFindAllWordsPrepare(TransitionsTable * transTable)
+CUDPPHandle scanplan1 = 0;
+CUDPPHandle scanplan2 = 0;
+int mem_size2 = sizeof( int) * 10240 * 1024;
+Buffer terminatedSymbolsBuf(mem_size2);
+Buffer d_odataBuf(mem_size2);
+Buffer pwordsCountBuf(sizeof(size_t)*2);
+
+Buffer wordsId(mem_size2);
+Buffer valid(mem_size2);
+Buffer keyWordsId(mem_size2);
+
+void deviceFindAllWordsPrepare(TransitionsTable * transTable, size_t len)
 {
 	cudaMalloc((void**)&table, transTable->FullSize);
 	
 	cudaMemcpy(table, transTable->Table, transTable->FullSize, cudaMemcpyHostToDevice);
-	
-}
-
-void deviceFindAllWords( char* text, int len, Word* words, int* count, int * allWords, int* allCount)
-{
-	// setup execution parameters
-	int threadsNum = 512;
-    dim3 threads(threadsNum, 1);
-    dim3 grid((len-1)/threadsNum+1,1);
-	int* terminatedSymbols;
-	int num_elements = len;
-	int mem_size = sizeof( int) * num_elements;
-	int sharedMemSize = threadsNum * sizeof(int);
-
-	cudaMalloc(&terminatedSymbols, mem_size);
-	device_MarkAllWords<<< grid, threads, sharedMemSize >>>(text, len, terminatedSymbols);
-	
-
-	// allocate device memory output arrays
-    int* d_odata = NULL;
-
-    cudaMalloc( (void**) &d_odata, mem_size);
 
 	CUDPPConfiguration config;	
 	config.datatype = CUDPP_INT;
 	config.algorithm = CUDPP_COMPACT;
 	config.options = CUDPP_OPTION_FORWARD | CUDPP_OPTION_INCLUSIVE |CUDPP_OPTION_INDEX;
     
-    CUDPPHandle scanplan = 0;
-    CUDPPResult result = cudppPlan(&scanplan, config, len, 1, 0); 
-	//Buffer wordsCountBuf(sizeof(size_t));
-	size_t* pwordsCount;
-    cudaMalloc( (void**) &pwordsCount, sizeof(size_t));
+    CUDPPResult result = cudppPlan(&scanplan1, config, len, 1, 0); 
 
-	cudppCompact(scanplan, d_odata, pwordsCount, text,(unsigned int*) terminatedSymbols, len);
+	CUDPPConfiguration config2;	
+
+	config2.datatype = CUDPP_INT;
+	config2.algorithm = CUDPP_COMPACT;
+	config2.options = CUDPP_OPTION_FORWARD | CUDPP_OPTION_INCLUSIVE ;	
+
+	result = cudppPlan(&scanplan2, config2, len, 1, 0); 
 	
-	//printf("Words count: %d \n", *(int*)(wordsCountBuf.GetHost()) );
-	//device_WatchDebug<<< 1, 1 >>>((char*)wordsCountBuf.GetDevice());
+}
 
-	Buffer wordsId(pwordsCount, sizeof(int));
-	Buffer valid(pwordsCount, sizeof(int));
-	Buffer keyWordsId(pwordsCount, sizeof(int));
-	cudppDestroyPlan(scanplan);
+void deviceFindAllWords( char* text, int len, Word* words, int* count, int * allWords, int* allCount)
+{
+	// setup execution parameters
+	int threadsNum = 128;
+    dim3 threads(threadsNum, 1);
+    dim3 grid((len-1)/threadsNum+1,1);
+	int* terminatedSymbols;
+	int num_elements = len;
+	int mem_size = sizeof( int) * num_elements;
+	int sharedMemSize = threadsNum * sizeof(int);
+//	Buffer terminatedSymbolsBuf(mem_size);
+	terminatedSymbols = (int*)terminatedSymbolsBuf.GetDevice();
+	
+	device_MarkAllWords<<< grid, threads, sharedMemSize >>>(text, len, terminatedSymbols);
+	
+
+	// allocate device memory output arrays
+	//Buffer d_odataBuf(mem_size);
+	int* d_odata = (int*)d_odataBuf.GetDevice();
+	
+	//Buffer pwordsCountBuf(sizeof(size_t));
+	size_t* pwordsCount = (size_t*)pwordsCountBuf.GetDevice();
+
+	cudppCompact(scanplan1, d_odata, pwordsCount, text,(unsigned int*) terminatedSymbols, len);
+	
+
+
+	//Buffer wordsId(pwordsCount, sizeof(int));
+	//Buffer valid(pwordsCount, sizeof(int));
+	//Buffer keyWordsId(pwordsCount, sizeof(int));
+
 		
 	device_FindAllWords<<< grid, threads, sharedMemSize >>>(table, text, len, d_odata, pwordsCount, (int*) wordsId.GetDevice() );
-	
-	scanplan = 0;
-	config.datatype = CUDPP_INT;
-	config.algorithm = CUDPP_COMPACT;
-	config.options = CUDPP_OPTION_FORWARD | CUDPP_OPTION_INCLUSIVE ;	
+		
+	unsigned int* w = (unsigned int*) wordsId.GetDevice();
 	int countWords;
 	cudaMemcpy( &countWords, pwordsCount, sizeof(int), cudaMemcpyDeviceToHost);
-	result = cudppPlan(&scanplan, config, (size_t)(countWords), 1, 0); 
-	unsigned int* w = (unsigned int*) wordsId.GetDevice();
-	
+
 	cudaMemcpy( valid.GetDevice(), wordsId.GetDevice(), countWords*sizeof(int), cudaMemcpyDeviceToDevice);
 	device_NormalizeAllWords<<< grid, threads >>>((unsigned int *)valid.GetDevice(), countWords);
-
-	cudppCompact(scanplan,( void*) keyWordsId.GetDevice(), pwordsCount, wordsId.GetDevice(), (unsigned int *)valid.GetDevice(), (size_t)(countWords));
-	cudppDestroyPlan(scanplan);
 	
-	cudaFree(d_odata);
+	
+	cudppCompact(scanplan2,( void*) keyWordsId.GetDevice(), pwordsCount, wordsId.GetDevice(), (unsigned int *)valid.GetDevice(), (size_t)(countWords));	
+	
 	cudaMemcpy(count, pwordsCount, sizeof(int), cudaMemcpyDeviceToDevice);
 	
 	//cudaFree(pwordsCount);
